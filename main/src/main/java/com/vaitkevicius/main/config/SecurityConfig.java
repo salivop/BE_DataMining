c74a23925eda76b74b1a55eb519a34b6ae1ba6fa
salivop <povilas.vaitkevicius@yahoo.com>
2018-10-18 19:24:14 +0300

    TESTING

--- main/src/main/java/com/vaitkevicius/main/config/SecurityConfig.java
+++ main/src/main/java/com/vaitkevicius/main/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+//TEST
 //    @Override
 //    public void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.inMemoryAuthentication().withUser("java")
